5d6ce243a03185ccc7831c0dea37ce3a22e942eb
Liliia Novikova <kplily85@gmail.com>
2020-09-15 20:58:26 +0300

    Сreates markup

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const textCategoryEl = document.querySelectorAll('.item h2');
+console.log(textCategoryEl.textContent);
+
+console.log('Категория:', textCategoryEl.textContent);
+const amountOfEl = document.querySelectorAll('.item li').length;
+console.log('Количество элементов:', amountOfEl);
+
+// const navEl = document.querySelector('#categories');
+
+// // const firstNavItemEl = navEl.querySelector('.site-nav__item');
+// const firstNavItemEl = navEl.firstElementChild;
+// console.log(firstNavItemEl.length);
+// console.log(navEl.children);
+// console.log(navEl.lastElementChild);
